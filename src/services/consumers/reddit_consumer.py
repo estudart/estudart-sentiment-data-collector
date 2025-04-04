05ac53068f0e6495ad51a11c201d52101f258f20
estudart <erico.studartdf@gmail.com>
2025-04-04 14:09:59 -0300

    feat: add transformer logic into app

--- src/services/consumers/reddit_consumer.py
+++ src/services/consumers/reddit_consumer.py
@@ -4,18 +4,25 @@ import time
 from multiprocessing import Process
 
 from src.queue.redis_queue_manager import RedisQueueManager
+from src.transformers.reddit_data_transformer import RedditDataTransformer
 from src.utils.extensions import logger
 from src.utils.config import secrets
 
 
 
-def consume_queue(queue_name):
+def consume_queue(keyword, data_type):
     consumer_instance = RedisQueueManager()
+    transformer_instance = RedditDataTransformer()
     while True:
+        queue_name = f"reddit-{keyword}-{data_type}"
         try:
             message = consumer_instance.consume_queue(queue_name)
             if message:
                 logger.debug(f"Received message on queue: {message}")
+                normalized_message = transformer_instance.transform_data(
+                    message, 
+                    data_type)
+                logger.info(f"Prepared data: {normalized_message}")
             else:
                 time.sleep(2)
         except KeyboardInterrupt as err:
@@ -26,8 +33,11 @@ def consume_queue(queue_name):
 
 
 if __name__ == "__main__":
-    for queue in secrets.get("QUEUES"):
+    for keyword in secrets.get("KEYWORDS"):
         posts_consumer_process = Process(target=consume_queue,
-                                        args=(queue,)
+                                        args=(keyword, "posts",)
+                                        ).start()
+        comments_consumer_process = Process(target=consume_queue,
+                                        args=(keyword, "comments",)
                                         ).start()
 
