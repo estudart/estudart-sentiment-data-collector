e0fd2568555f49ac6a9a772632f56e086f9c8b12
estudart <erico.studartdf@gmail.com>
2025-04-04 14:10:22 -0300

    feat: import vars from config

--- src/services/scrappers/reddit_scrapper.py
+++ src/services/scrappers/reddit_scrapper.py
@@ -1,17 +1,20 @@
 import sys
+from multiprocessing import Process
 
 from src.collectors.reddit_collector import RedditDataCollector
+from src.utils.config import secrets
 
 
+def start_scrapper(keyword):
+    reddit_instance = RedditDataCollector()
+    reddit_instance.run(keyword)
 
 if __name__ == "__main__":
     try:
-        reddit_instance = RedditDataCollector()
-
-        reddit_instance.run(
-            "cryptocurrency",
-            limit=5
-        )
+        for keyword in secrets.get("KEYWORDS"):
+            Process(target=start_scrapper, 
+                    args=(keyword, )
+                    ).start()
     except KeyboardInterrupt as err:
         print("\nGracefully exiting app...")
         sys.exit()
