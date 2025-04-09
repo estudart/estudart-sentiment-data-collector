8d8369019c5965cc2b2cd15fcd0b153d712918cb
estudart <erico.studartdf@gmail.com>
2025-04-09 16:18:33 -0300

    feat: enhance process capacity

--- src/services/consumers/youtube/app.py
+++ src/services/consumers/youtube/app.py
@@ -13,43 +13,44 @@ from src.utils.config import secrets
 
 
 
-def consume_queue(keyword, data_type):
+def consume_queue(asset_keyword_list, data_type):
     consumer_instance = RedisQueueManager()
-    queue_name = f"youtube-{keyword}-{data_type}"
+    
     while True:
-        try:
-            message = consumer_instance.consume_queue(queue_name)
-            if message:
-                message["created_utc"] = datetime.strptime(message.get("created_utc"),
-                                                           '%Y-%m-%dT%H:%M:%SZ')
-                logger.debug(f"Received message on queue: {message}")
-                
-                if data_type == "videos":
-                    new_element = YoutubeVideo(**message)
-                elif data_type == "comments":
-                    new_element = YoutubeComment(**message)
-                try:
-                    logger.info(f"Prepared data: {message}")  
-                    session.add(new_element)
-                    session.commit()
-                except Exception as err:
-                    session.rollback()
-                    logger.error(f"Error saving post: {err}")
-            else:
-                time.sleep(2)
-        except KeyboardInterrupt as err:
-            logger.info("Gracefully exiting app...")
-            sys.exit()
-        except Exception as err:
-            logger.info(f"Could not consume queue, reason: {err}")
+        for keyword in asset_keyword_list:
+            queue_name = f"youtube-{keyword}-{data_type}"
+            try:
+                message = consumer_instance.consume_queue(queue_name)
+                if message:
+                    message["created_utc"] = datetime.strptime(message.get("created_utc"),
+                                                            '%Y-%m-%dT%H:%M:%SZ')
+                    logger.debug(f"Received message on queue: {message}")
+                    
+                    if data_type == "videos":
+                        new_element = YoutubeVideo(**message)
+                    elif data_type == "comments":
+                        new_element = YoutubeComment(**message)
+                    try:
+                        logger.info(f"Prepared data: {message}")  
+                        session.add(new_element)
+                        session.commit()
+                    except Exception as err:
+                        session.rollback()
+                        logger.error(f"Error saving post: {err}")
+                else:
+                    time.sleep(2)
+            except KeyboardInterrupt as err:
+                logger.info("Gracefully exiting app...")
+                sys.exit()
+            except Exception as err:
+                logger.info(f"Could not consume queue, reason: {err}")
 
 
 if __name__ == "__main__":
     for asset, asset_keyword_list in secrets.get("YOUTUBE_QUERYS").items():
-        for keyword in asset_keyword_list:
-            posts_consumer_process = Process(target=consume_queue,
-                                            args=(keyword, "videos",)
-                                            ).start()
-            comments_consumer_process = Process(target=consume_queue,
-                                            args=(keyword, "comments",)
-                                            ).start()
+        posts_consumer_process = Process(target=consume_queue,
+                                        args=(asset_keyword_list, "videos",)
+                                        ).start()
+        comments_consumer_process = Process(target=consume_queue,
+                                        args=(asset_keyword_list, "comments",)
+                                        ).start()
